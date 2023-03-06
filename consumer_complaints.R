d091d9e9310cf678f9aabf5e7fb586c30590aa0d
lukeacowan <lukecowan20@augustana.edu>
2023-03-05 18:27:53 -0600

    Shiny app visualization and final changes

--- consumer_complaints.R
+++ consumer_complaints.R
@@ -12,13 +12,16 @@ library(plotly)
 library(shiny)
 library(DT)
 library(zoo)
+library(anytime)
+library(data.table)
 rm(list = ls())
 
 setwd("C:/Users/lukec/OneDrive/Documents/data332/sentiment_analysis_project/Complaint_Sentiment_Analysis/data")
 
+#reduced file size by converting csv to rds
 raw_data <- readRDS(file = "Consumer_Complaints.rds")
 
-#brought in columns deemed my and made naming conventions consistent - left out tags, zip codes, and complaint IDs; also left out date received because date sent appeared to be the more accurate column
+#brought in columns deemed necessary and made naming conventions consistent - left out tags, zip codes, and complaint IDs; also left out date received because date sent appeared to be the more accurate date column
 #also deselected consumer consent, company public response, submission type, and consumer dispute columns
 my_data <- raw_data%>%
   dplyr::select("Product", "Sub.product", "Issue", "Sub.issue", "Consumer.complaint.narrative",
@@ -51,7 +54,7 @@ complaint_analysis <- valid_data %>%
   arrange(desc(n)) %>%
   #filtering out insignificant words such as "of" or "the"
   anti_join(stop_words) %>%
-  #removing censored information and mispellings
+  #removing censored information and misspellings
   dplyr::filter(word != "xxxx",
                 word != "xx",
                 word != "n't",)
@@ -163,41 +166,69 @@ plot_geo(state_sentiment, locationmode = 'USA-states') %>%
   add_trace(
     z = state_sentiment$overall_sentiment, locations = state_sentiment$State)
 
-response_sentiment <- narrative_text %>%
-  #appending the bing sentiment dictionary
-  inner_join(get_sentiments("bing")) %>% 
-  #counting by product and sentiment
-  count(Company.response.to.consumer, sentiment) %>% 
-  #spreading the sentiment and count columns
-  spread(sentiment, n) %>% 
-  #compute overall_sentiment
-  mutate(overall_sentiment = positive - negative) %>%
-  arrange(overall_sentiment) 
+#setting up data frame for shiny app visualization  
+date_sent <- setDT(valid_data) %>%
+  #reformatting Date.sent column to enable splitting  
+  .[,Date.sent := as.Date(Date.sent, format = "%m/%d/%Y")] %>%
+  .[,Year.sent := year(Date.sent)] %>%
+  .[,Month.sent := month(Date.sent)] %>%
+  #combining year and month values and adding a "0" for single-digit month values
+  .[,Year.month := ifelse(nchar(Month.sent) == 2, paste0(Year.sent, Month.sent), paste0(Year.sent, "0",Month.sent))] %>%
+  #grouping the number of complaints based on the year, month, and product values associated with them
+  .[,num_complaints := .N, keyby = .(Year.sent, Month.sent, Product)]
 
+#removing unnecessary columns and displaying distinct values
+date_sent <- unique(date_sent[,.(Year.sent, Month.sent, Product, Year.month, num_complaints)])
+
+ggplot(date_sent, aes(x = Year.month, y = num_complaints, group = Product))+
+  geom_line(aes(colour = Product), lwd=1.0) + 
+  xlab("Decade") + 
+  ylab("Proportion of Total Ladybugs Found") + 
+  ggtitle("Proportion of Ladybugs Found by Decade") +
+  theme(axis.text.x = element_text(angle = 270, hjust = 1))
+
+#shiny app visualization
 ui<-fluidPage( 
   
   titlePanel(title = "Explore Consumer Complaints Dataset"),
-  h4('Complaints by day'),
+  h4('Complaints by Month per Product'),
   
+  #setting up data entry/selection areas for user
   fluidRow(
     column(2,
-           selectInput('Company Response', 'Choose Response', response_sentiment$Company.response.to.consumer)
+           selectizeInput('Product_choice', 'Choose Product', date_sent$Product, multiple = TRUE, selected = "Credit card"),
+           selectInput('Start_date', 'Choose Start Date', date_sent$Year.month),
+           selectInput('End_date', 'Choose End Date', date_sent$Year.month),
+           #used to display results for desired inputs
+           actionButton("Button", "Go")
     ),
-    column(4,plotOutput('plot_01')),
-    column(6,DT::dataTableOutput("table_01", width = "100%"))
+    column(4,plotOutput('plot_01', width = "1540px", height = "800px"))
+
   )
   
   
 )
 
 server<-function(input,output){
+  #passing on inputs from the ui to the server to enable graphing
+  observeEvent(input$Button, {  
+  date_sent <- date_sent[Product %in% input$Product_choice]
+  date_sent <- date_sent[between(as.numeric(Year.month), input$Start_date, input$End_date)]
   
-  output$plot_01 <- renderPlot({
-    ggplot(response_sentiment, aes(x = Company.response.to.consumer, y = overall_sentiment, fill = Company.response.to.consumer)) +
-      geom_col(data = subset(response_sentiment, Company.response.to.consumer == input$Company.response.to.consumer))
-  })
+
+  #creating time series analysis
+  product_line_plot <- 
+    ggplot(date_sent, aes(x = Year.month, y = num_complaints, group = Product))+
+      geom_line(aes(colour = Product), lwd=1.0) + 
+      xlab("Year and Month") + 
+      ylab("Complaints Submitted") + 
+      ggtitle("Complaints Submitted Each Month by Product") +
+      theme(axis.text.x = element_text(angle = 270, hjust = 1))
   
-  output$table_01<-DT::renderDataTable(valid_data[,c(input$X,input$Y,input$Splitby)],options = list(pageLength = 4))
+
+  output$plot_01 <- renderPlot({product_line_plot})
+  }) 
+  
 }
 
 shinyApp(ui=ui, server=server) 
